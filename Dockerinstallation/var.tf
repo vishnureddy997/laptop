d5659405a89d935fcaaec7435905a87123cefcb3
vishnureddy997 <57907899+vishnureddy997@users.noreply.github.com>
2021-08-04 17:25:42 +0530

    Add files via upload

---
+++ Dockerinstallation/var.tf
@@ -0,0 +1,24 @@
+variable "access_key" {
+  default = "AKIAWVHBCL6MUUPL7R7P"
+
+}
+
+variable "ami" {
+  default = "ami-0c1a7f89451184c8b"
+
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+
+}
+variable "cidr_blocks" {
+    default = "["0.0.0.0/0"]"
+  
+}
+
+variable "user" {
+    default = "ubuntu"
+  
+}
+  
